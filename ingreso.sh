ee4fd634e95ebe747624cb1e663f24199754c0e1
maiimaimaimai <mainoicm@gmail.com>
2024-11-04 21:26:21 -0300

    parte 1, login

---
+++ ingreso.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+login() {
+echo "Nombre de usuario:"
+read userName
+while [ "$userName" == "" ]; do 
+    echo "No se ingresó nombre de usuario"
+    echo "Vuelva a ingresar nombre de usuario:"
+    read userName
+done
+
+echo "Contrasena:"
+read password
+while [ "$password" == "" ]; do
+    echo "No se ingresó contrasena"
+    echo "Vuelva a ingresar contrasena:"
+    read password
+done
+
+
+if grep -q "^$userName $password$" admin_usuarios.txt; then
+    echo "admin"
+elif grep -q "^$userName $password$" clientes_usuarios.txt; then
+    echo "cliente"
+else 
+    echo "Credenciales incorrectas, vuelva a intentar."
+    login
+fi
+}
+
+menu_cliente() {
+echo "1- Mascotas disponibles para adopción"
+echo "2- Adoptar mascota"
+echo "3- Salir"
+read option
+while [ "$option" != "1" ] || [ "$option" != "2" ] || [ "$option" != "3" ]; do
+    echo "Opción no válida, vuelva a ingresar opción"
+    read option
+done
+
+if $option == "1"; then
+}
+
+login
+menu_cliente
